a114bc31d7a8de7e474b50a0243b72ddee7217c3
jovnc <95868357+jovnc@users.noreply.github.com>
2025-06-16 00:59:37 +0700

    Minor UI improvements (#6)

--- apps/frontend/src/components/chat/message-list.tsx
+++ apps/frontend/src/components/chat/message-list.tsx
@@ -1,13 +1,21 @@
 "use client";
 import MessageBubble from "./message-bubble";
-import type { UIMessage, ChatRequestOptions } from "ai";
+import type { UIMessage } from "ai";
 import WelcomeScreen from "./welcome-sceen";
+import { Button } from "../ui/button";
+import { Repeat } from "lucide-react";
 
 interface MessageListProps {
   messages: UIMessage[];
+  error: any;
+  reload: () => void;
 }
 
-export default function MessageList({ messages }: MessageListProps) {
+export default function MessageList({
+  messages,
+  error,
+  reload,
+}: MessageListProps) {
   return (
     <div className="flex-1 overflow-y-auto">
       <div className="max-w-3xl mx-auto px-4 py-6">
@@ -21,6 +29,21 @@ export default function MessageList({ messages }: MessageListProps) {
               <MessageBubble key={message.id} message={message} />
             ))}
           </>
+        )}
+
+        {/* Display error message if any */}
+        {error && (
+          <div className="w-full text-center text-sm text-muted-foreground mt-4 space-y-2">
+            <div className="text-destructive">An error occurred.</div>
+            <Button
+              type="button"
+              onClick={() => reload()}
+              variant={"ghost"}
+              size="sm"
+            >
+              <Repeat />
+            </Button>
+          </div>
         )}
       </div>
     </div>
